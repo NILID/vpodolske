b2473f84415ac99335d46e3eb4341a11fdfc2c0e
da <dm.ilin.aleksandrovich@gmail.com>
2020-01-26 23:06:01 +0300

    remove unused js functions

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -46,70 +46,14 @@ jQuery(function ($) {
     });
 
     /*==============================================================*/
-    // Search Slide
-    /*==============================================================*/
-   (function() {
-
-        $('.search-icon').on('click', function() {
-            $('.searchNlogin').toggleClass("expanded");
-        });
-    }());
-
-
-    /*==============================================================*/
     // TheiaStickySidebar
     /*==============================================================*/
 
    (function() {
-
         $('.tr-sticky')
             .theiaStickySidebar({
                 additionalMarginTop: 0
             });
     }());
-
-
-	/*==============================================================*/
-	// Animationend
-	/*==============================================================*/
-
-
-    (function( $ ) {
-
-        //Function to animate slider captions 
-        function doAnimations( elems ) {
-            //Cache the animationend event in a variable
-            var animEndEv = 'webkitAnimationEnd animationend';
-
-            elems.each(function () {
-                var $this = $(this),
-                    $animationType = $this.data('animation');
-                $this.addClass($animationType).one(animEndEv, function () {
-                    $this.removeClass($animationType);
-                });
-            });
-        }
-
-        //Variables on page load
-        var $myCarousel = $('#home-carousel'),
-            $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-
-        //Initialize carousel
-        $myCarousel.carousel();
-
-        //Animate captions in first slide on page load 
-        doAnimations($firstAnimatingElems);
-
-        //Pause carousel
-        $myCarousel.carousel('pause');
-
-        //Other slides to be animated on carousel slide event 
-        $myCarousel.on('slide.bs.carousel', function (e) {
-            var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-            doAnimations($animatingElems);
-        });
-
-    })(jQuery);
-
 // script end
 });
