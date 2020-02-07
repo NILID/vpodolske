74401dc3fca42a522ddf61dae0e397d4b2354bae
DA <da@vpodolske.com>
2020-02-07 15:11:59 +0300

    Remove users block from main_page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,12 +5,8 @@ class MainController < ApplicationController
     @ev_today = Event.where('eventdate = ? or (eventdate < ? AND finishdate > ?)', today, today, today)
                      .where(hidden: false)
                      .sample(6)
-    @users = User.where.not(confirmed_at: nil).includes(:profile).order(created_at: :desc).limit(6)
     @articles = Article.order('created_at desc').limit(5)
     @comments = Comment.shown.includes(:user, :commentable).order(created_at: :desc).limit(5)
-    # NEWS
-    # source = 'http://inpodolsk.ru/novosti?page=1'
-    # @page = Nokogiri::HTML(open(source.to_s))
   end
 
   def sponsor; end
