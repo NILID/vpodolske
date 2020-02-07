56da132b0769c53f8de4794c506315b7404d71b5
DA <da@vpodolske.com>
2020-02-07 15:36:07 +0300

    Rebuild news blocks

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
     @ev_today = Event.where('eventdate = ? or (eventdate < ? AND finishdate > ?)', today, today, today)
                      .where(hidden: false)
                      .sample(6)
-    @articles = Article.order('created_at desc').limit(5)
+    @articles = Article.order('created_at desc').limit(4)
     @comments = Comment.shown.includes(:user, :commentable).order(created_at: :desc).limit(5)
   end
 
--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 describe MainController do
   it 'should index' do
-    create_list(:user, 10)
-
     get :index
 
     expect(assigns(:ev_today)).to be_empty
-    expect(assigns(:users)).not_to be_empty
 
     expect(response).to render_template(:index)
   end
